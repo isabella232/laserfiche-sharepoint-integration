4d58d4787f2c4326bde717a20102beb4a01e5d8d
rfulton-lf <robert.fulton@laserfiche.com>
2023-10-11 06:00:07 -0700

    added the pricing link to admin config web part

--- src/webparts/laserficheAdminConfiguration/components/HomePage/HomePage.tsx
+++ src/webparts/laserficheAdminConfiguration/components/HomePage/HomePage.tsx
@@ -2,10 +2,21 @@ import * as React from 'react';
 require('./../../../../Assets/CSS/commonStyles.css');
 require('../../../../Assets/CSS/bootstrap.min.css');
 
+const YOU_MUST_BE_CLOUD_USER_TO_USE_WEB_PART =
+  'You must be a currently licensed Laserfiche Cloud user to use this web part.';
+const FOR_MORE_INFO_VISIT = 'For more information visit';
+
 export default function HomePage(): JSX.Element {
   return (
     <div className='p-3'>
       <main className='bg-white shadow-sm'>
+        <p className='adminContent'>
+          {`${YOU_MUST_BE_CLOUD_USER_TO_USE_WEB_PART} ${FOR_MORE_INFO_VISIT} `}
+          <a href='https://www.laserfiche.com/products/pricing'>
+            laserfiche.com
+          </a>
+          {`.`}
+        </p>
         <p className='adminContent'>
           Use the Laserfiche Administration page to edit your SharePoint and
           Laserfiche configuration. Sign in and select the task you want to
@@ -17,7 +28,7 @@ export default function HomePage(): JSX.Element {
             href='https://laserfiche.github.io/laserfiche-sharepoint-integration/'
             target='_blank'
             rel='noreferrer'
-            style={{color: '#0079d6'}}
+            style={{ color: '#0079d6' }}
           >
             help documentation.
           </a>
@@ -31,8 +42,8 @@ export default function HomePage(): JSX.Element {
               Profiles govern how documents in SharePoint will be saved to
               Laserfiche. You can create multiple profiles for different
               SharePoint content types. For example, if you want applications
-              stored differently than invoices, create separate
-              profiles for each content type.
+              stored differently than invoices, create separate profiles for
+              each content type.
             </span>
           </p>
           <p>
